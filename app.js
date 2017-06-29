2a667e3b1107c676a97f98b95f5ee0f9ec581a66
martinbinhoudek <martinbinhoudek@gmail.com>
2017-06-29 21:27:18 +0200

    Mongodb in one permanent instance

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var app=express();
 var util=require('util');
 var mongo=require('mongodb');
 var MongoClient=mongo.MongoClient;
+var db;
 var url="mongodb://localhost:27017/mydb";
-
 
 /**
 * Loads all contact data based on http get request on /
@@ -12,15 +12,9 @@ var url="mongodb://localhost:27017/mydb";
 app.get('/', function (req, res) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  
-  MongoClient.connect(url, function(err,db) {
+  db.collection("contacts").find().toArray(function (err, result) {
     if (err) throw err;
-    console.log("Database connected");
-	
-	db.collection("contacts").find().toArray(function (err, result) {
-	  if (err) throw err;
-	  res.send(result);
-	});
+    res.send(result);
   });
 });
 
@@ -31,21 +25,14 @@ app.get('/delete', function(req, res) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   console.log('Trying delete branch');
-  console.log(req.query.id);
-  
-  MongoClient.connect(url, function(err,db) {
+  console.log(req.query.id);  
+  var o_id=new mongo.ObjectID(req.query.id);
+  var myquery= { "_id" : o_id };
+  db.collection("contacts").deleteOne(myquery, function (err, obj) {
     if (err) throw err;
-    console.log("Database connected");
-	
-	var o_id=new mongo.ObjectID(req.query.id);
-	var myquery= { "_id" : o_id };
-	db.collection("contacts").deleteOne(myquery, function (err, obj) {
-	  if (err) throw err;
-	  console.log(req.query.id + " deleted");
-	  db.close();
-	});
-	  res.send("OK");
-	});
+	console.log(req.query.id + " deleted");
+  });
+  res.send("OK");
 });
 
 /**
@@ -56,20 +43,12 @@ app.get('/save', function(req,res) {
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   console.log('Trying save branch');
   console.log(req.query.name);
-  
-  MongoClient.connect(url, function(err,db) {
-    if (err) throw err;
-    console.log("Database connected");
-	
-	db.collection("contacts").insertOne(req.query, function (err, obj) {
-	  if (err) throw err;
-	  console.log(req.query + " saved");
-	  db.close();
-	});
-	  res.send("Saved");  
-	});
+  db.collection("contacts").insertOne(req.query, function (err, obj) {
+	if (err) throw err;
+	console.log(req.query + " saved");
+  });
+  res.send("Saved");  
 });
-
 
 /**
 * Saves changes made to the record based on http get request on /edit
@@ -79,21 +58,20 @@ app.get('/edit', function(req,res) {
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   console.log('Trying edit branch');
   console.log(req.query.id);
-  
-  MongoClient.connect(url, function(err,db) {
-    if (err) throw err;
-    console.log("Database connected");
-	var o_id=new mongo.ObjectID(req.query.id);
-	var myquery= { "_id" : o_id };
-	db.collection("contacts").updateOne(myquery, req.query, function (err, obj) {
-	  if (err) throw err;
-	  console.log(req.query + " edited");
-	  db.close();
-	});
-	  res.send("Edited");
-	});
+  var o_id=new mongo.ObjectID(req.query.id);
+  var myquery= { "_id" : o_id };
+  db.collection("contacts").updateOne(myquery, req.query, function (err, obj) {
+	if (err) throw err;
+	console.log(req.query + " edited");
+  });
+  res.send("Edited");
 });
 
-app.listen(8080, function() {
-  console.log('App listening on port 8080');
+MongoClient.connect(url, function(err,database) {
+  if (err) throw err;
+  db=database;
+  console.log("Database connected");
+  app.listen(8080, function() {
+    console.log('App listening on port 8080');
+  });
 });
